46da676dace80b323a23983457879d7a10e5f0e9
theheking <36276388+theheking@users.noreply.github.com>
2026-02-26 11:43:42 +0000

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Isoform Specific Perturb-Seq'
+project = 'scDATSS'
 copyright = '2026, Helen King'
 author = 'Helen King'
 
